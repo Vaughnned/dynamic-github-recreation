32dabc744c9e8ef4c8d7e1a3c91ced2409242e06
Vaughn Nedderman <vaughn@nedderman.com>
2023-06-16 16:05:06 -0400

    attempting to fix displaying fetched data

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const renderRepos = () => {
-  const container = document.querySelector("#repo-container");
-  repos.forEach((repo) => {
-    let listItem = document
-      .querySelector("#repo-title")
-      .content.cloneNode(true);
-    listItem.appendChild(document.createTextNode(repo));
-    container.appendChild(listItem);
-  });
-};
+// const renderRepos = () => {
+//   const container = document.querySelector("#repos-list");
+//   repos.forEach((repo) => {
+//     let listRepo = document
+//       .querySelector("#repo-title")
+//       .content.cloneNode(true);
+//     listRepo.appendChild(document.createTextNode(repo));
+//     container.appendChild(listRepo);
+//   });
+// };
 
-const getRepos = async () => {
+const getResources = async () => {
   const request = await fetch(
     "https://api.github.com/users/vaughnned/repos?sort=updated_at"
   );
   const response = await request.json();
-  response.forEach((obj) => repos.push(obj.name));
-  console.log(repos);
-  renderRepos();
+  response.forEach((obj) => {
+    repos.push(obj.name);
+  });
+  console.log(response);
+  // renderRepos();
 };
 
 let repos = [];
-getRepos();
+getResources();
