0a166c5864f1a73f635887cb99cf01b1e42d2fcc
Vaughn Nedderman <vaughn@nedderman.com>
2023-06-20 16:51:14 -0400

    finished fixing my display

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const renderRepos = () => {
-  const container = document.querySelector("#repos-list");
-  repos.forEach((repo) => {
-    let listRepo = document.getElementById("repoList");
-    listRepo.appendChild(document.createTextNode(repo));
-  });
-  // console.log(listRepo);
-};
+// const renderRepos = () => {
+//   const container = document.querySelector("#repos-list");
+//   repos.forEach((repo) => {
+//     let listRepo = document.getElementById("repoList");
+//     listRepo.appendChild(document.createTextNode(repo));
+//   });
+//   // console.log(listRepo);
+// };
 
-const getResources = async () => {
-  const request = await fetch(
-    "https://api.github.com/users/vaughnned/repos?sort=updated_at"
-  );
-  const response = await request.json();
-  response.forEach((obj) => {
-    repos.push(obj.name);
-  });
-  console.log(response);
-  renderRepos();
-};
+// const getResources = async () => {
+//   const request = await fetch(
+//     "https://api.github.com/users/vaughnned/repos?sort=updated_at"
+//   );
+//   const response = await request.json();
+//   response.forEach((obj) => {
+//     repos.push(obj.name);
+//   });
+//   console.log(response);
+//   renderRepos();
+// };
 
-let repos = [];
-getResources();
+// let repos = [];
+// getResources();
